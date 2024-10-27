becf3f9be0b5eb30eb00f371b26bf3e03ebf2b61
Raffy Seyhan <raffyseyhan889@gmail.com>
2024-10-27 10:35:26 +0800

    main.py

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ def cek_akun_terdaftar(nama):
         with open(csv_file, mode="r", newline="", encoding="utf-8") as akun_file:
             reader = csv.DictReader(akun_file)
             for row in reader:
-                if row['nama'] == nama:
+                if row["nama"] == nama:
                     return True
         return False
     except FileNotFoundError:
-
         return False
 
 def simpan_data_akun(nama, password):
@@ -30,13 +29,11 @@ def simpan_data_akun(nama, password):
     try:
         with open(csv_file, "a", newline="", encoding="utf-8") as akun:
             writer = csv.DictWriter(akun, fieldnames=dataheader_akun)
-            
             akun.seek(0, 2)  
             if akun.tell() == 0:  
                 writer.writeheader()
-                
             writer.writerows(data_akun)  
-        print("Berhasil melakukan registrasi!")
+        print("Data berhasil disimpan!")
     except Exception as e:
         print(f"Terjadi kesalahan: {e}")
 
@@ -48,32 +45,48 @@ def menu_login():
         print("3. Keluar Program")
         pil_1 = input("Masukan Pilihan: ")
 
-        if pil_1 == "1" :
+        if pil_1 == "1":
             print("===== Login ======")
-            nama = input("Masukan nama: ")
-            password = pwinput.pwinput("Masukan Password: ")
-            return nama, password
-        
-        if pil_1 == "2":
+            while True:
+                nama = input("Masukan nama: ")
+                if nama == "":
+                    print("Nama tidak boleh kosong")
+                    continue
+                if any(char in simbol for char in nama) or any(char in angka for char in nama):
+                    print("Nama tidak boleh mengandung angka atau simbol")
+                    continue
+                else:
+                    password = pwinput.pwinput("Masukaan Password: ")
+                    if password == "":
+                        print("Password tidak boeh kosong")
+                    
+
+        elif pil_1 == "2":
             print("==== Register ====")
             while True:
                 nama = input("Masukan nama: ")
+                if nama == "":
+                    print("Nama tidak boleh kosong")
+                    continue
                 if any(char in simbol for char in nama) or any(char in angka for char in nama):
                     print("Nama tidak boleh mengandung angka atau simbol. Coba lagi.")
                     continue
+                if cek_akun_terdaftar(nama):
+                    print("Nama sudah terdaftar! Coba nama lain.")
+                    continue
                 else:
                     password = pwinput.pwinput("Masukan Password: ")
+                    if password == "":
+                        print("Password tidak boleh kosong")
+                        continue
                     simpan_data_akun(nama, password)
                     break
-        
+
         elif pil_1 == "3":
             print("Keluar Program.")
             exit()
-            
+
         else:
             print("Pilihan tidak valid, coba lagi.")
 
 nama, password = menu_login()
-
-
-
